e6bc83369c79b82b2ebbe4acb87b857fc8e7a467
Sonwabile_Shumane <89164763+S00s00@users.noreply.github.com>
2025-02-16 10:31:22 +0000

    working with generic types

--- src/main/java/com/linkedin/collections/Application.java
+++ src/main/java/com/linkedin/collections/Application.java
@@ -7,12 +7,14 @@ public class Application {
 
 	public static void main(String[] args) {
 		
-		Collection c = new ArrayList(); //Same Collection Type, Different Element Type
-		c.add("Cambridge");
+		Collection<Room> c = new ArrayList<>(); //Same Collection Type, Different Element Type
+		c.add(new Room("Cambridge", "Suite", 6, 250.0));
+
+		c.stream().forEach(e->System.out.println(e.getName()));
 		
-		Collection c2 = new ArrayList(); //Same Collection Type, Different Element Type
+		Collection<Room> c2 = new ArrayList<>(); //Same Collection Type, Different Element Type
 		c2.add(new Room("Cambridge", "Suite", 6, 250.0));
-		
+		c2.stream().forEach(e->System.out.println(e.toString()));
 	}
 
 }
