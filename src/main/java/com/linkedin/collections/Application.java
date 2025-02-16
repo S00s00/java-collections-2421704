9b349a1f5d2ada4dd1cf832fe0e7ebbbf20bea70
Sonwabile_Shumane <89164763+S00s00@users.noreply.github.com>
2025-02-16 14:44:09 +0000

    using lambda in streams

--- src/main/java/com/linkedin/collections/Application.java
+++ src/main/java/com/linkedin/collections/Application.java
@@ -3,8 +3,6 @@ package com.linkedin.collections;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.function.Consumer;
-import java.util.function.Predicate;
 
 public class Application {
 
@@ -19,23 +17,27 @@ public class Application {
 		oxford.setPetFriendly(true);
 		victoria.setPetFriendly(true);
 		
-		rooms.stream()
-			.filter(new Predicate<Room>() {
+		// rooms.stream()
+		// 	.filter(new Predicate<Room>() {
 
-				@Override
-				public boolean test(Room room) {
-					System.out.format("Testing %s with result %b%n", room.getName(), room.isPetFriendly());
-					return room.isPetFriendly();
-				}
+		// 		@Override
+		// 		public boolean test(Room room) {
+		// 			System.out.format("Testing %s with result %b%n", room.getName(), room.isPetFriendly());
+		// 			return room.isPetFriendly();
+		// 		}
 				
-			}).forEach(new Consumer<Room>() {
+		// 	}).forEach(new Consumer<Room>() {
 
-				@Override
-				public void accept(Room room) {
-					System.out.println(room.getName());
-				}
+		// 		@Override
+		// 		public void accept(Room room) {
+		// 			System.out.println(room.getName());
+		// 		}
 				
-			});
+		// 	});
+
+		rooms.stream().
+		filter(Room :: isPetFriendly). //-> room.isPetFriendly()).
+		forEach(room ->System.out.println(room.getName()));
 		
 	}
 }
