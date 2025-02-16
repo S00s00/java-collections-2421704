d185dcaf3ae62176a83c23af4d1227193df7cd72
Sonwabile_Shumane <89164763+S00s00@users.noreply.github.com>
2025-02-16 16:29:53 +0000

    challenge on iterating collections

--- src/main/java/com/linkedin/collections/RoomService.java
+++ src/main/java/com/linkedin/collections/RoomService.java
@@ -17,22 +17,26 @@ public class RoomService {
 	public void applyDiscount(final double discount) {
 		
 		//Reduces the rate of each room by the provided discount
-	
+		this.inventory.forEach(r -> r.setRate(r.getRate() * (1 - discount)));
 	}
 
 	public Collection<Room> getRoomsByCapacity(final int requiredCapacity) {
 		
 		//Returns a new collection of rooms that meet or exceed the provided capacity
-		
-		return null;
-		
+		Collection<Room> matches =  new HashSet<>();
+		for(Room room : inventory){
+			if(room.getCapacity() >= requiredCapacity) matches.add(room);
+		}
+		return matches;
 	}
 	
 	public Collection<Room> getRoomByRateAndType(final double rate, final String type){
 	
 		//Returns a new collection of rooms with a rate below the provided rate and that match the provided type
 		
-		return null;
+		return this.inventory.stream().
+		filter(r->r.getRate() < rate)
+		.filter(r -> r.getType().equals(type)).collect(Collectors.toList());
 		
 	}
 	
