7edc1e477f49717f320a05cec8e97c7ebb3e93a8
Sonwabile_Shumane <89164763+S00s00@users.noreply.github.com>
2025-02-16 10:23:43 +0000

    creating a collection

--- src/main/java/com/linkedin/collections/Application.java
+++ src/main/java/com/linkedin/collections/Application.java
@@ -1,10 +1,19 @@
 package com.linkedin.collections;
+import java.util.ArrayDeque;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
 
 public class Application {
 
 	public static void main(String[] args) {
+		Collection<String> c =  new ArrayDeque<>();
+		c.addAll(List.of("snowboard","linux","books","snowboard"));
+		System.out.println(c);
+		Collection<String> favoriteThings = new HashSet<>(c);
+		System.out.println(favoriteThings);
 		
-		
+
 	}
 
 }
