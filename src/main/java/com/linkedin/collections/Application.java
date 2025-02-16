3661fbd6d4e6f34af3797d8a9dbc8594fb0925f2
Sonwabile_Shumane <89164763+S00s00@users.noreply.github.com>
2025-02-16 16:55:50 +0000

    Treesetd and its methods

--- src/main/java/com/linkedin/collections/Application.java
+++ src/main/java/com/linkedin/collections/Application.java
@@ -2,12 +2,22 @@ package com.linkedin.collections;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.NavigableSet;
+import java.util.TreeSet;
 
 public class Application {
 
 	public static void main(String[] args) {
 		
 		List<Integer> numbers = Arrays.asList(500, 1500, 2500, 1000, 3000, 2000);
-		
+		NavigableSet<Integer> numberTree = new TreeSet<>(numbers);
+
+		numberTree.stream().forEach(System.out::println); // streams all elemets of tree set
+		System.out.println("\n");
+		numberTree.headSet(1750).stream().forEach(System.out::println); // streams elements less then head set
+		System.out.println("\n");
+		numberTree.tailSet(1750).stream().forEach(System.out::println);// streams elements greater then tail set
+		System.out.println("\n");
+		numberTree.subSet(1000,2500).stream().forEach(System.out::println); // streams a subset
 	}
 }
