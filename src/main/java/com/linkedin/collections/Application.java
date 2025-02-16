3278e612604899222aef23352dc2dc4ac8700d56
Sonwabile_Shumane <89164763+S00s00@users.noreply.github.com>
2025-02-16 16:43:48 +0000

    workingg  with hashsets

--- src/main/java/com/linkedin/collections/Application.java
+++ src/main/java/com/linkedin/collections/Application.java
@@ -1,5 +1,9 @@
 package com.linkedin.collections;
 
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Application {
 
 	public static void main(String[] args) {
@@ -9,6 +13,14 @@ public class Application {
 		Room oxfordDuplicate = new Room("Oxford", "Suite", 5, 225.0);
 		Room victoria = new Room("Victoria", "Suite", 5, 225.00);
 
+		Set<Room> otherRooms = Set.of(piccadilly,oxford); // unmodifiable
+		
+		Set<Room> rooms =  new LinkedHashSet<>(); //HashSet<>() does not garentee order
+		rooms.addAll(List.of(piccadilly,oxford,oxford,oxfordDuplicate,victoria));
+
+		Set<Room> moreRooms = Set.copyOf(rooms); // also unmordifiable
+
+		moreRooms.stream().map(r->r.getName()).forEach(System.out::println);
 		
 	}
 }
