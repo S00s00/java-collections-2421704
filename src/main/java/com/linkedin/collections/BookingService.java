abf1ce3a3d6a87613011d28eb8710d9b722b65b6
Sonwabile_Shumane <89164763+S00s00@users.noreply.github.com>
2025-02-17 09:46:46 +0000

    challenge for maps

--- src/main/java/com/linkedin/collections/BookingService.java
+++ src/main/java/com/linkedin/collections/BookingService.java
@@ -17,8 +17,8 @@ public class BookingService {
 		 * Returns a boolean that indicates if the Guest was
 		 * successfully placed in the room.
 		 */
-		
-		return false;
+			
+		return this.bookings.putIfAbsent(room, guest) == null;
 	}
 
 	public double totalRevenue() {
@@ -27,7 +27,7 @@ public class BookingService {
 		 * 2. Returns a double that totals the rate of each Room booked
 		 * in the bookings Map.
 		 */
-		return 0;
+		return this.bookings.keySet().stream().mapToDouble(Room::getRate).sum();
 	}
 	
 	public Map<Room, Guest> getBookings() {
